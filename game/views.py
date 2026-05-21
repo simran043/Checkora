12bcf97879c9b6242d35309caa75cd2682817870
Snehal Nakil <snehalit24@gmail.com>
2026-05-21 19:49:17 +0530

    feat: add responsive hamburger menu for mobile rulebook

--- game/views.py
+++ game/views.py
@@ -174,6 +174,7 @@ def new_game(request):
 
     request.session['game'] = game.to_dict()
     request.session.modified = True
+    request.session.save()
 
     return JsonResponse({
         'valid': True,
